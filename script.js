e868ba4fdf143d257771590d2065c6e926463b9a
nyland23 <144254600+nyland23@users.noreply.github.com>
2023-09-11 12:41:40 -0400

    commit 3.2

--- script.js
+++ script.js
@@ -21,26 +21,43 @@ CALCULAR.addEventListener('click', () => {
     function calcFlujo(peso){
         let resto = peso;
         let flujo = 0;
-        let superficie = 0
         if (resto<=10){
                 flujo= resto*100;
             }
         else{
             if (resto<=20){
-                flujo=10*100+((20-resto)*50);
+                flujo=10*100+((resto-10)*50);
             }
             else{ 
                 if (resto<=30){
-                flujo=10*100+(10*50)+((30-resto)*20);
-                }
-                else{ 
-                    if (resto>30){
-                    let superficie = ((resto*4)+7)/(resto+90);
-                    flujo = superficie*1500;
-                    }
+                flujo=10*100+(10*50)+((resto-20)*20);
                 }
             }
         }
         return flujo;
-    }
+    }   
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').valueAsNumber
+        if (DATO > 0 && DATO <= 30){
+            ERROR.style.display = 'none'
+            let flujo = calcFlujo(DATO);
+            let mantenimiento = flujo*1.5;
+            FLU.innerHTML = flujo + ' cc/hr';
+            MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+            FLU.style.display = 'block';
+            MAN.style.display = 'block';
+        }
+         else if (DATO>30){
+            let SuperficieCorporal = ( (DATO * 4) + 7) / (DATO + 90);
+            FLU.innerHTML = SuperficieCorporal*1500 + ' cc/hr' ;
+            MAN.innerHTML = SuperficieCorporal*2000 + ' cc/hr' ;
+            FLU.style.display = 'block';
+            MAN.style.display = 'block';
+        } 
+            else {
+                ERROR.style.display = 'block';
+                FLU.style.display = 'none';
+                MAN.style.display = 'none';       
+            }
+    })
     
